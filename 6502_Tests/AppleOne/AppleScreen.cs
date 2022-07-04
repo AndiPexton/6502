5098bab7e05a18f75c9cc88ff0219a610abcf3c2
andipextonhome <andi.pexton@googlemail.com>
2022-07-05 00:12:57 +0100

    Added functions for the screen to change colour

--- 6502_Tests/AppleOne/AppleScreen.cs
+++ 6502_Tests/AppleOne/AppleScreen.cs
@@ -1,17 +1,23 @@
 ﻿using System.Text;
 using Abstractions;
+using Shadow.Quack;
 
 namespace AppleOne;
 
 public class AppleScreen : IOverLay
 {
-       
+    private ushort screen = 0xD012;
+    private ushort screenRegister = 0xD013;
+    private ushort fgAddress = 0xD014;
+    private ushort bgAddress = 0xD015;
+    private ushort cursorX = 0xD016;
+    private ushort cursorY = 0xD017;
+
     public AppleScreen()
     {
         Start = 0xD012;
-        End = 0xD012;
+        End = 0xD017;
         Console.CursorVisible = true;
-        Console.CursorSize = 1;
     }
 
     public int Start { get; }
@@ -19,22 +25,60 @@ public class AppleScreen : IOverLay
 
     public void Write(ushort address, byte b)
     {
-        if (b == 155) return;
-        b = (byte)(b & 0b01111111);
-           
-        if (b == 13)
+        if (address == screenRegister)
         {
-            Console.WriteLine();
+            if (b == 0)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            if (b == 1)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
         }
-        else
+
+
+        if (address == screen)
         {
-            var format = Encoding.ASCII.GetString(new[] { b });
-            Console.Write(format);
+            if (b == 155) return;
+            b = (byte)(b & 0b01111111);
+
+            if (b == 13)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                var format = Encoding.ASCII.GetString(new[] { b });
+                Console.Write(format);
+            }
         }
+
+        if (address == fgAddress)
+            Console.ForegroundColor = Duck.CastToEnum<ConsoleColor>(b);
+        if (address == bgAddress)
+            Console.BackgroundColor = Duck.CastToEnum<ConsoleColor>(b);
+        if (address == cursorX)
+            Console.CursorLeft = b;
+        if (address == cursorY)
+            Console.CursorTop = b;
     }
-    
+
     public byte Read(ushort address)
     {
+        if (address == bgAddress)
+            return (byte)Console.BackgroundColor;
+        if (address == fgAddress)
+            return (byte)Console.ForegroundColor;
+        if (address == cursorX)
+            return (byte)Console.CursorLeft;
+        if (address == cursorY)
+            return (byte)Console.CursorTop;
+
         return 0;
     }
 }
